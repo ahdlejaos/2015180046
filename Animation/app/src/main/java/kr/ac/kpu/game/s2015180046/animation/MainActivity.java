5d4feafc59709d653c6a90bdbf7b34a59b566ffe
강성모 <ahdlejaos123@gmail.com>
2021-03-29 04:30:12 +0900

    4_Animation

---
+++ Animation/app/src/main/java/kr/ac/kpu/game/s2015180046/animation/MainActivity.java
@@ -0,0 +1,30 @@
+package kr.ac.kpu.game.s2015180046.animation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"onCreate");
+    }
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        Log.d(TAG,"onConfigChange:"+newConfig);
+        super.onConfigurationChanged(newConfig);
+    }
+
+
+
+}
