0520743f23381ccd14c4d9202d589fffcd65db87
KangSungMo <ahdlejaos@naver.com>
2021-03-29 11:57:13 +0900

    doGameFrame,Updating

--- SampleGame/app/src/main/java/com/example/samplegame/GameView.java
+++ SampleGame/app/src/main/java/com/example/samplegame/GameView.java
@@ -6,32 +6,65 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
-    private final Bitmap bitmap;
+    private static final String TAG = GameView.class.getSimpleName();
+
+    private Bitmap bitmap;
+//   변수형 final은 생성자에서 모두값이결정되야함
+    private float x;
+    private float y;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         Resources res = getResources();
 // XML 에서지정한것들을 로드할때 getResources 사용
-// activity나 view가 가지고있은 getResources 쓰면댐
+// activity나 view가 가지고있는 getResources 쓰면댐
+        initResources(res);
+        startUpdating();
+
+    }
+
+    private void startUpdating() {
+        doGameFrame();
+//        
+//        1이벤트처리 -> 2계산 -> 3그림
+        
+    }
+
+    private void doGameFrame() {
+        x += 0.01;
+        y += 0.01;
+        
+//        update();        
+//        draw();
+        invalidate();
+
+
+    }
+
+    private void initResources(Resources res) {
         bitmap = BitmapFactory.decodeResource(res,R.mipmap.soccer_ball_240);
 //                resource ID를가지고  BITMAP 생성해줌
-
-
-
-
+        x = 100;
+        y = 100;
+//ondraw가 불리도록하는함수
+//timer() 특정코드를 준비해놓고 이코드를 얼마후에 실행시켜주는함수
+//        refacter - extract method
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 //        부모가 가지고있는함수 검색하면 오버라이드가능
 //        super.onDraw(canvas);  부모부르는거
-        canvas.drawBitmap(bitmap,0,0,null);
+        canvas.drawBitmap(bitmap,x,y,null);
 //        화면에bitmap인자받은거보여줌
+
+        Log.d(TAG,"Drawing at:"+x+","+y);
 
     }
 }
